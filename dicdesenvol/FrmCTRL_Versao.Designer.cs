1ff811efc45f96087e4db924bc965e3318b490b2
LUCSALE <luciano@lucsale.com.br>
2025-07-21 15:45:05 -0300

    Ultima Ajustes - Versão: 1.20

---
+++ dicdesenvol/FrmCTRL_Versao.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace dicdesenvol
+{
+    partial class FrmCTRL_Versao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlHeader = new Guna.UI2.WinForms.Guna2Panel();
+            this.lblTitulo = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.pnlGrid = new Guna.UI2.WinForms.Guna2Panel();
+            this.pnlDados = new Guna.UI2.WinForms.Guna2Panel();
+            this.pnlFooter = new Guna.UI2.WinForms.Guna2Panel();
+            this.pnlHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.Controls.Add(this.lblTitulo);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(717, 61);
+            this.pnlHeader.TabIndex = 0;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblTitulo.Location = new System.Drawing.Point(199, 3);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(319, 57);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "CTRL_Versão";
+            // 
+            // pnlGrid
+            // 
+            this.pnlGrid.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlGrid.Location = new System.Drawing.Point(0, 61);
+            this.pnlGrid.Name = "pnlGrid";
+            this.pnlGrid.Size = new System.Drawing.Size(200, 431);
+            this.pnlGrid.TabIndex = 1;
+            // 
+            // pnlDados
+            // 
+            this.pnlDados.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlDados.Location = new System.Drawing.Point(200, 61);
+            this.pnlDados.Name = "pnlDados";
+            this.pnlDados.Size = new System.Drawing.Size(517, 431);
+            this.pnlDados.TabIndex = 2;
+            // 
+            // pnlFooter
+            // 
+            this.pnlFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlFooter.Location = new System.Drawing.Point(200, 458);
+            this.pnlFooter.Name = "pnlFooter";
+            this.pnlFooter.Size = new System.Drawing.Size(517, 34);
+            this.pnlFooter.TabIndex = 3;
+            // 
+            // FrmCTRL_Versao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(717, 492);
+            this.Controls.Add(this.pnlFooter);
+            this.Controls.Add(this.pnlDados);
+            this.Controls.Add(this.pnlGrid);
+            this.Controls.Add(this.pnlHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmCTRL_Versao";
+            this.Text = "FrmCTRL_Versao";
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel pnlHeader;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblTitulo;
+        private Guna.UI2.WinForms.Guna2Panel pnlGrid;
+        private Guna.UI2.WinForms.Guna2Panel pnlDados;
+        private Guna.UI2.WinForms.Guna2Panel pnlFooter;
+    }
+}
---
+++ dicdesenvol/FrmCTRL_Versao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dicdesenvol
+{
+    public partial class FrmCTRL_Versao : Form
+    {
+        public FrmCTRL_Versao()
+        {
+            InitializeComponent();
+        }
+    }
+}
